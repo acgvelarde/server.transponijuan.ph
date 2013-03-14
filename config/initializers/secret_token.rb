300fa53b2e220aff6622f4b6018e9ca353748e39
Allejo Chris Velarde <chris.velarde@chromedia.com>
2013-03-14 10:14:46 +0800

    added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerTransponijuanPh::Application.config.secret_token = 'c1644da95beadde6aec1d0698a8c01592c7a893d6ed309f93d92f3ffe7aa7e04ad40da77f42f07b3ebfcd55600c6ab8540b4ece30b7c359a268370eb8469a25f'
